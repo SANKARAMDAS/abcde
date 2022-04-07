a2c2e2211ccca3230d632680fce5a6c778905d17
Sankaram <das95sankaram@gmail.com>
2022-04-07 15:09:10 +0530

    export reactArrowFunctionExportComponent

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React from 'react'
+import { Resturent} from './component/Basics/Resturent'
 
 const App = () => {
   return (
-    <>
-    <MyApp />
-    <MyApp />
-      <h1 className='myclass'>
-      Hello World! Welcome to my Fresh ReactJs Learning.
-      </h1>
-      <MyApp />
-      <p>Awesome</p>
-    </>
-  )
-};
-
-const MyApp = () => {
-  return (
-    <h1>My</h1>
+    <Resturent />
   )
 }
-
 
 export default App
