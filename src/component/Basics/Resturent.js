2a20e6f42ffbfd9fd04fd8ed7bf5472db3260d80
Sankaram <das95sankaram@gmail.com>
2022-04-07 15:09:47 +0530

    React Arrow Function Export Component

---
+++ src/component/Basics/Resturent.js
@@ -0,0 +1,17 @@
+// import React from 'react'
+
+// const Resturent = () => {
+//   return (
+//     <>Resturent</>
+//   )
+// }
+
+// export default Resturent
+
+import React from 'react'
+
+export const Resturent = () => {
+  return (
+    <div>Resturent</div>
+  )
+}
